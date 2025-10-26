b8f18e26d5701017a9baf28a3364fcb81307476f
Shivanshu19droid <shivanshumishra2003@gmail.com>
2025-10-27 00:17:55 +0530

    Stepping into community module - group and post models added

---
+++ Server/models/group.model.js
@@ -0,0 +1,37 @@
+import {Schema, mdoel} from "mongoose";
+import mongoose from "mongoose";
+
+const groupSchema = new Schema ({
+    name: {
+        type: String,
+        required: [true, "Group name is required"],
+        minLength: [5, "Group name must contain at least 5 characters"],
+        maxLength: [50, "Group name cannot exceed 50 characters"],
+        trim: true,
+        unique: [true, "Group name already exists"]
+    },
+    description: {
+        type: String,   
+    },
+    icon: {
+        public_id: {
+            type: String
+        },
+        secure_url: {
+            type: String
+        }
+    },
+    admin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    members: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+});
+
+const Group= model("Group", groupSchema);
+
+export default Group;
---
+++ Server/models/post.model.js
@@ -0,0 +1,52 @@
+import {Schema, model} from "mongoose";
+import mongoose from "mongoose";
+
+const postSchema = new Schema({
+    content: {
+        type: String,
+        maxLength: [3000, "Post cannot exceed 3000 characters"]
+    },
+    image: {
+        public_id: {
+            type: String
+        },
+        secure_url: {
+            type: String
+        }
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    group: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Group"
+    },
+    likes: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    comments: {
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        content: {
+            type: String,
+            maxLength: [1000, "Comment cannot exceed 1000 characters"]
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Post = model("Post", postSchema);
+
+export default Post;
